a9c65387f8cdbea904502e6f000945e979671eef
Vassilis Zafeiris <bzafiris@gmail.com>
2022-07-22 15:07:54 +0300

    Updated build script

---
+++ grant-permissions.bat
@@ -0,0 +1,4 @@
+adb shell pm grant com.gabm.fancyplaces android.permission.WRITE_EXTERNAL_STORAGE
+adb shell pm grant com.gabm.fancyplaces android.permission.ACCESS_FINE_LOCATION
+adb shell pm grant com.gabm.fancyplaces android.permission.ACCESS_COARSE_LOCATION
+adb shell pm grant com.gabm.fancyplaces android.permission.ACCESS_BACKGROUND_LOCATION
